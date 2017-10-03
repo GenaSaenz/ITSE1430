4b1f002b08df2bb40c194a6a1251057e9e7f6f53
gena.saenz <gena.saenz@my.tccd.edu>
2017-10-02 19:08:45 -0500

    10/2/17 Class Updates

--- ClassWork/Section 2/Nile/Nile.Windows/MainForm.Designer.cs
+++ ClassWork/Section 2/Nile/Nile.Windows/MainForm.Designer.cs
@@ -28,18 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MI1File = new System.Windows.Forms.ToolStripMenuItem();
+            this._miFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this._miProductAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this._miProductEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this._miProductDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._miHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // menuStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MI1File,
+            this.toolStripMenuItem2,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(580, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MI1File
+            // 
+            this.MI1File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miFileExit});
+            this.MI1File.Name = "MI1File";
+            this.MI1File.Size = new System.Drawing.Size(37, 20);
+            this.MI1File.Text = "File";
+            // 
+            // _miFileExit
+            // 
+            this._miFileExit.Name = "_miFileExit";
+            this._miFileExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this._miFileExit.Size = new System.Drawing.Size(152, 22);
+            this._miFileExit.Text = "Exit";
+            this._miFileExit.Click += new System.EventHandler(this.OnFileExit);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miProductAdd,
+            this.toolStripSeparator1,
+            this._miProductEdit,
+            this.toolStripSeparator2,
+            this._miProductDelete});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Overflow = System.Windows.Forms.ToolStripItemOverflow.AsNeeded;
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(66, 20);
+            this.toolStripMenuItem2.Text = "Products";
+            // 
+            // _miProductAdd
+            // 
+            this._miProductAdd.Name = "_miProductAdd";
+            this._miProductAdd.Size = new System.Drawing.Size(152, 22);
+            this._miProductAdd.Text = "Add";
+            this._miProductAdd.Click += new System.EventHandler(this.OnProductAdd);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // _miProductEdit
+            // 
+            this._miProductEdit.Name = "_miProductEdit";
+            this._miProductEdit.Size = new System.Drawing.Size(152, 22);
+            this._miProductEdit.Text = "Edit";
+            this._miProductEdit.Click += new System.EventHandler(this.OnProductEdit);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // _miProductDelete
+            // 
+            this._miProductDelete.Name = "_miProductDelete";
+            this._miProductDelete.Size = new System.Drawing.Size(152, 22);
+            this._miProductDelete.Text = "Delete";
+            this._miProductDelete.Click += new System.EventHandler(this.OnProductDelete);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miHelpAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // _miHelpAbout
+            // 
+            this._miHelpAbout.Name = "_miHelpAbout";
+            this._miHelpAbout.Size = new System.Drawing.Size(152, 22);
+            this._miHelpAbout.Text = "About";
+            this._miHelpAbout.Click += new System.EventHandler(this.OnHelpAbout);
             // 
             // MainForm
             // 
@@ -47,16 +135,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(580, 334);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "MainForm";
             this.Text = "Nile";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MI1File;
+        private System.Windows.Forms.ToolStripMenuItem _miFileExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem _miProductAdd;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem _miProductEdit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem _miProductDelete;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _miHelpAbout;
     }
 }
 
--- ClassWork/Section 2/Nile/Nile.Windows/MainForm.cs
+++ ClassWork/Section 2/Nile/Nile.Windows/MainForm.cs
@@ -10,16 +10,57 @@ namespace Nile.Windows
             InitializeComponent();
         }
 
-        private void button1_Click( object sender, EventArgs e )
-        {
-            var product = new Product();
-            product.Name = "Product A";
 
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void OnProductAdd( object sender, EventArgs e )
+        {
             var child = new ProductDetailForm("Product Details");
             if (child.ShowDialog(this) != DialogResult.OK)
                 return;
 
-            //var product = child.Product;
+            //TODO: Save product
+            _product = child.Product;
         }
+
+        private void OnProductEdit( object sender, EventArgs e )
+        {
+            var child = new ProductDetailForm("Product Details");
+            child.Product = _product;
+            if (child.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            //TODO: Save product
+            _product = child.Product;
+        }
+
+        private void OnProductDelete( object sender, EventArgs e )
+        {
+            if (_product == null)
+                return;
+
+            //Confirm
+            if (MessageBox.Show(this, $"Are you sure you want to delete '{_product.Name}'?",
+                  "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                return;
+
+            //TODO: Delete product
+            _product = null;
+        }
+
+        private Product _product;
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            var about = new AboutBox();
+            about.ShowDialog(this);
+        }
+
+
+
     }
 }
